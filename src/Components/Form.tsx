ea1e095ee4823abf6f404d04d298bb1f666edb1c
Patrick Henegan <phenegan03@gmail.com>
2023-03-01 15:44:03 -0500

    added some basic CSS, updated the Question interface

---
+++ src/Components/Form.tsx
@@ -0,0 +1,8 @@
+import './Form.css';
+
+
+export default function Form() {
+    return (
+        <div className = "form">Project Group Matching</div>
+    );
+}
--- src/Model/QuizQuestion.ts
+++ src/Model/QuizQuestion.ts
@@ -1,3 +1,6 @@
+import { JsxElement } from "typescript";
+import { QuizAnswer } from "./Types";
+
 /**
  * Represents a Question for a form or quiz. For the purpose of this application,
  * there will likely need to be some way of quantifying the answer,
@@ -7,6 +10,8 @@ export default interface QuizQuestion {
     get prompt(): string;
     set prompt(question: string);
 
-    get answer(): string;
-    set answer(answer: string);
+    get answer(): QuizAnswer;
+    set answer(answer: QuizAnswer);
+
+    view(): JsxElement;
 }
