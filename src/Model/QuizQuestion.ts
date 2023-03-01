c6ac4087d6f79c84e7af35b1397b1d458ef9c993
Patrick Henegan <phenegan03@gmail.com>
2023-03-01 14:59:19 -0500

    started working on model for the project

---
+++ src/Model/QuizQuestion.ts
@@ -0,0 +1,12 @@
+/**
+ * Represents a Question for a form or quiz. For the purpose of this application,
+ * there will likely need to be some way of quantifying the answer,
+ * because it will have to be processed in some way in order create a matching
+ */
+export default interface QuizQuestion {
+    get prompt(): string;
+    set prompt(question: string);
+
+    get answer(): string;
+    set answer(answer: string);
+}
